0ae3c299c4316b378151428fec98a9e2a61990cf
Asaduzzaman04 <asadchy152@gmail.com>
2024-11-05 20:12:52 +0600

    animation added in card

--- 10_crud_with_restApi/crud_RestApi/src/api/authApi.js
+++ 10_crud_with_restApi/crud_RestApi/src/api/authApi.js
@@ -15,6 +15,10 @@ export const axiosDelete = async (id) => {
 //post.get('') function is return Promise and post the data
 export const axiosPost = async (data) => {
   // return api.post(`posts/${data}`); // Incorrect URL Formatting: By using posts/${data}, you are attempting to add the data object directly into the URL path. This will convert data to the string "[object Object]" (or similar), which results in an incorrect URL, leading to the 404 Not Found error.
-
   return api.post("/posts", data); //   Corrected line
 };
+
+//update post 
+export const axiosUpdate =  async (UPDATE) =>{
+  return api.put("/posts"/ UPDATE)
+}
--- 10_crud_with_restApi/crud_RestApi/src/components/ui/ItemCard.jsx
+++ 10_crud_with_restApi/crud_RestApi/src/components/ui/ItemCard.jsx
@@ -1,9 +1,17 @@
 import Button from "./Button";
-
+import { motion } from "framer-motion";
 const ItemCard = ({ data, handleDelete }) => {
   return (
     <>
-      <li className="hover:scale-105 hover:shadow-[0_15px_50px_rgba(8,_112,_184,_0.7)] transition-all duration-200 ease-linear h-full px-2 py-3 md:px-3 md:py-4 rounded-md bg-sky-950">
+      <motion.li 
+      initial={{opacity: 0, y:-20,}}
+      animate={{opacity: 1,}}
+      whileInView={{y: 1}}
+      drag="y"
+      transition={{delay:data.id / 90,}}
+      dragConstraints={{bottom: 20, top: -20,}}
+      whileDrag={{scale: 1,cursor:"grabbing",scaleX:1.1}}
+      className="hover:scale-105 hover:shadow-[0_15px_50px_rgba(8,_112,_184,_0.7)] transition-all duration-200 ease-linear h-full px-2 py-3 md:px-3 md:py-4 rounded-md bg-sky-950">
         <div className="flex flex-col gap-5 font-semibold ">
           <p>{data.id}</p>
           <div className="flex flex-col gap-2">
@@ -20,7 +28,7 @@ const ItemCard = ({ data, handleDelete }) => {
             </button>
           </div>
         </div>
-      </li>
+      </motion.li>
     </>
   );
 };
